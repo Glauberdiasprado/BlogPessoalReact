9aa12bded44527fb5acba0c8010e98f5627778ce
Glauberdiasprado <glauberdiasprado@gmail.com>
2022-08-16 13:08:42 -0300

    implementando alerts personalizados

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -69,34 +69,35 @@ function Login() {
         }
     }
 
-  return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-      <Grid alignItems="center" xs={6}>
-        <Box paddingX={20}>
-          <form onSubmit={onSubmit}>
-            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="texto1" >Entrar</Typography>
-            <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="usuario" variant="outlined" name="usuario" margin="normal" fullWidth />
-            <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth />
-            <Box marginTop={2} textAlign="center">
-                <Button type="submit" variant="contained" color="primary">
-                  Logar
-                </Button>
-            </Box>
-          </form>
-          <Box display="flex" justifyContent="center" marginTop={2}>
-            <Box marginRight={1}>
-              <Typography variant="subtitle1" gutterBottom align="center">Não tem uma conta?</Typography>
-            </Box>
-            <Link to="/cadastrousuario"><Typography variant="subtitle1" gutterBottom align="center" className="texto1" >Cadastre-se</Typography></Link>
+    return (
+      <Grid container direction='row' justifyContent='center' alignItems='center'>
+          <Grid alignItems='center' xs={6}>
+              <Box paddingX={20}>
+                  <form onSubmit={onSubmit}>
+                      <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
+                      <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                      <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                      <Box marginTop={2} textAlign='center'>
+                          <Button type='submit' variant='contained' color='primary'>
+                              Logar
+                          </Button>
+                      </Box>
+                  </form>
+                  <Box display='flex' justifyContent='center' marginTop={2}>
+                      <Box marginRight={1}>
+                          <Typography variant='subtitle1' gutterBottom align='center' >Não tem uma conta?</Typography>
+                      </Box>
 
-          </Box>
-        </Box>
-      </Grid>
-      <Grid xs={6} className="imagem"   //serve para repetir a imagem quando vezes preferir ou não repetir a imagem
-      >
+                      <Link to='/cadastrousuario'>
+                          <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastra-se</Typography>
+                      </Link>
+                  </Box>
+              </Box>
+          </Grid>
+          <Grid xs={6} className='imagem'>
 
+          </Grid>
       </Grid>
-    </Grid>
   );
 }
 
